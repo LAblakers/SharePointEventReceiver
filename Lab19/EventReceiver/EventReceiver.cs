043e6265a0539d788e9da9bc3c71a3bed893332d
Blake Fermaniuk <doomguy_54@hotmail.com>
2019-06-13 21:22:03 -0600

    Importing project.

---
+++ Lab19/EventReceiver/EventReceiver.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+using System.IO;
+using System.Linq;
+namespace Lab19.EventReceiver
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class EventReceiver : SPItemEventReceiver
+    {
+
+        /// <summary>
+        /// An item is being added.
+        /// </summary>
+        /// 
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            
+
+            using (SPSite siteCollection = new SPSite("http://mysites.innotech.cloud/sites/edmonton"))
+            {
+
+                
+                //Collection of lists fom the Site
+                SPWeb topSite = siteCollection.RootWeb;
+                SPList list1 = topSite.Lists["NotificationEnabled"];
+                SPListItemCollection items1 = list1.Items;
+                int c = 0;
+                int[] arrayitems = new int[list1.ItemCount];
+
+                foreach (SPListItem item in items1) //Email recipients list                       
+                {
+                    arrayitems[c] = item.ID;   
+                    c++;
+                }
+     
+                int maxValue = arrayitems.Max();
+                var itemID = maxValue;
+
+                SPListItem notificationEnabled = list1.GetItemByIdSelectedFields(itemID, "EmailRecipient");                
+                var list1EmailReceipt = notificationEnabled["EmailRecipient"];
+
+                SPList list2 = topSite.Lists["Email Recipients"];
+                SPListItemCollection items = list2.Items;
+
+                //Iterate through each list item on both lists to find matching list items!
+                using (StreamWriter w = File.AppendText(@"C:\\Testing" + "\\" + "Log_File_Name.txt"))
+                {
+                        
+                        foreach (SPListItem item in items) //Email recipients list                       
+                        {
+
+                            SPListItem emailRecipient = list2.GetItemByIdSelectedFields(item.ID, "EmailAddress");
+                            var list2EmailReceipt = emailRecipient["EmailAddress"];
+
+                            if (list2EmailReceipt != null)
+                            {
+                                if (list1EmailReceipt.ToString().Trim().ToUpper() == (list2EmailReceipt.ToString().Trim().ToUpper()))
+                                {
+                           
+                                    w.WriteLine("An email would have been sent to " + item["EmailAddress"].ToString() + " at {0} {1} ", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString());
+                                }
+                                 
+                            }
+                            
+                        }
+                    
+                }
+            }
+        }
+
+        public override void ItemUpdated(SPItemEventProperties properties)
+        {
+            using (SPSite siteCollection = new SPSite("http://mysites.innotech.cloud/sites/edmonton"))
+            {
+
+
+                //Collection of lists fom the Site
+                SPWeb topSite = siteCollection.RootWeb;
+                SPList list1 = topSite.Lists["NotificationEnabled"];
+                SPListItemCollection items1 = list1.Items;
+                int c = 0;
+                int[] arrayitems = new int[list1.ItemCount];
+
+                foreach (SPListItem item in items1) //Email recipients list                       
+                {
+                    arrayitems[c] = item.ID;
+                    c++;
+                }
+
+                int maxValue = arrayitems.Max();
+                var itemID = maxValue;
+
+                SPListItem notificationEnabled = list1.GetItemByIdSelectedFields(itemID, "EmailRecipient");
+                var list1EmailReceipt = notificationEnabled["EmailRecipient"];
+
+                SPList list2 = topSite.Lists["Email Recipients"];
+                SPListItemCollection items = list2.Items;
+
+                //Iterate through each list item on both lists to find matching list items!
+                using (StreamWriter w = File.AppendText(@"C:\\Testing" + "\\" + "Log_File_Name.txt"))
+                {
+
+                    foreach (SPListItem item in items) //Email recipients list                       
+                    {
+
+                        SPListItem emailRecipient = list2.GetItemByIdSelectedFields(item.ID, "EmailAddress");
+                        var list2EmailReceipt = emailRecipient["EmailAddress"];
+
+                        if (list2EmailReceipt != null)
+                        {
+                            if (list1EmailReceipt.ToString().Trim().ToUpper() == (list2EmailReceipt.ToString().Trim().ToUpper()))
+                            {
+
+                                w.WriteLine("An email would have been sent to " + item["EmailAddress"].ToString() + " at {0} {1} ", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString());
+                            }
+
+                        }
+
+                    }
+
+                }
+            }
+        }  
+    }
+}
+    
---
+++ Lab19/Features/Feature1/Feature1.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="27bd08f1-dca6-49cd-b6bd-ecf5d19884cd" featureId="27bd08f1-dca6-49cd-b6bd-ecf5d19884cd" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Lab19 Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="848ff8dc-dccc-48eb-9096-e06213bc261f" />
+  </projectItems>
+</feature>
